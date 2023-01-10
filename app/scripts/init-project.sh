05d6fa99c1e2550cbb5929b6996e780af23ce534
Chinkit Patel <chinkit@Chinkits-MBP.lan>
2023-01-10 20:06:53 +1030

    Version update
    adding this commit

--- app/scripts/init-project.sh
+++ app/scripts/init-project.sh
@@ -6,6 +6,6 @@ rill init --project ${PROJECT_BASE}/${PROJECT}
 # rill import-table ${PROJECT_BASE}/${PROJECT}/data/2016/meta_cells.csv --project ${PROJECT_BASE}/${PROJECT}
 for file in ${PROJECT_BASE}/data/2016/*.csv
 do
-    rill import-table "$file" --project ${PROJECT_BASE}/${PROJECT}
+    rill source add "$file" --project ${PROJECT_BASE}/${PROJECT}
 done
 
